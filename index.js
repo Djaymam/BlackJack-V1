9358bfb87e4d911c62bff8ac541bcdc8ac29c5fe
Jaylson Almeida <j.almeidacv@gmail.com>
2022-06-19 16:01:51 -0100

    cleaning code

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-let cards=[]
-let sum=0
-let blackJack=false
-let gameStatus=true
-let message=""
-
-
-
+let cards = []
+let sum = 0
+let blackJack = false
+let gameStatus = true
+let message = ""
+let player = {
+   name: "Jaylson",
+   Chip: 100
+}
 //funcao para trazer randomizadas
 function getRandonCard(){
-   let randomNumb=Math.floor(Math.random()*14)+1
-   if (randomNumb===1){//convertendo Ace to 11 
+   let randomNumb = Math.floor(Math.random()*14)+1
+   if (randomNumb === 1){//convertendo Ace to 11 
       return 11
-   }else if(randomNumb>10){//convertendo DAMA REI e VALETE para 10
+   }else if(randomNumb > 10){//convertendo DAMA REI e VALETE para 10
       return 10
    }else{
       return randomNumb
@@ -22,48 +23,48 @@ function getRandonCard(){
 function startGame(){
    let firstCard = getRandonCard()
    let secondCard = getRandonCard()
-   sum=firstCard+secondCard
-   cards=[firstCard,secondCard]
+   sum = firstCard+secondCard
+   cards = [firstCard,secondCard]
    renderNewGame()
 }
 //funcao do jogo base
 function renderNewGame(){
-    let messageEl=document.getElementById("message-el")
-    let sumEl=document.getElementById("sum-el")
-    let cardsEl=document.getElementById("cards-el")
-    cardsEl.innerHTML="Cards: "+cards+" "
-    sumEl.innerHTML="Sum: "+sum
+    let playerData = document.getElementById("player-el")
+    let messageEl = document.getElementById("message-el")
+    let sumEl = document.getElementById("sum-el")
+    let cardsEl = document.getElementById("cards-el")
+    playerData.innerHTML = player.name + ": $ " + player.Chip
+    cardsEl.innerHTML = "Cards: "+cards+" "
+    sumEl.innerHTML = "Sum: "+sum
 
  if(sum===21){
-   message="BLACK JACK"
-   blackJack=true
-   gameStatus=false
+   message = "BLACK JACK"
+   blackJack = true
    window.alert("GANHOU!!!!")
 
- }else if(sum<21){
-    message="Do You Want To Draw New Cards?"
-    gameStatus=true
+ }else if(sum <= 20){
+    message = "Do You Want To Draw New Cards?"
+
  }else{
-    message="You are out of the GAME!!"
-    gameStatus=false
+    message = "You are out of the GAME!!"
+    gameStatus = false
     window.alert("PERDEUU!!")
  }
- messageEl.innerHTML=message
+ messageEl.innerHTML = message
 }
 //Funcao para obter nova carta
 function newCard(){
-   if(blackJack===false || gameStatus===true){
-      message.innerHTML="Drawing New Card From The Deck!"
-      let drawCards=getRandonCard()
-      sum+=drawCards
+   if(blackJack === false && gameStatus === true){
+      
+      message.innerHTML = "Drawing New Card From The Deck!"
+      let drawCards = getRandonCard()
+      sum += drawCards
       cards.push(drawCards)
       //cardsEl.innerHTML=cards+" "
       //cardsEl.innerHTML="Cards: "+firstCard+" "++" "+drawCards
       //sumEl.innerHTML="Sum: "+sum
       //renderizar os novos dados 
      renderNewGame()
-
-   }else{
 
    }
 }
